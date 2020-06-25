d082ca2aaa1010410ff8a61fae1fd358a58a7dfd
dunay2 <34416258+dunay2@users.noreply.github.com>
2020-06-25 23:12:24 +0100

    Adding Owners List and updated controller . Closes #.16

--- pet-clinic-data/src/main/java/com/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/sfgpetclinic/services/OwnerService.java
@@ -3,7 +3,5 @@ package com.sfgpetclinic.services;
 import com.sfgpetclinic.model.Owner;
 
 public interface OwnerService extends CrudService<Owner, Long> {
-
     Owner findByLastName(String lastName);
-
 }
