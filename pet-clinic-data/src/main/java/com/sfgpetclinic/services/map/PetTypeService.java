0d013f5bbf34761221e4868d0076141320322adb
dunay2 <34416258+dunay2@users.noreply.github.com>
2020-06-26 19:30:29 +0100

    Added PetTypeService . Closes #29.

---
+++ pet-clinic-data/src/main/java/com/sfgpetclinic/services/map/PetTypeService.java
@@ -0,0 +1,8 @@
+package com.sfgpetclinic.services.map;
+
+import com.sfgpetclinic.model.PetType;
+import com.sfgpetclinic.services.CrudService;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
+
